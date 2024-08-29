d4922042a7af9059b9c3fb8793fd553b1e75e8e9
Sanny2306 <rastegaev.90@bk.ru>
2024-08-29 19:53:19 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+name = "Alexander"
+print ( name  )
+name = "Homme_work 3"
+print( name )
+name = "Time spent 1.5"
+print( name )
+name = "Python"
+print( name )
+name = "Time_for_one_Task 0.5"
+print( name )
+print("Alexander,Курс Python,Homme_Work 3,Time spent 1.5,Time for one Task 0.5")
