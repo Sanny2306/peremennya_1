24bd1bacce2d4a1852738bcf3a3adb8a6d2f77af
Sanny2306 <rastegaev.90@bk.ru>
2024-09-01 17:52:39 +0300

    исправленая домашняя работа!

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ HommeWork = 12
 print (HommeWork)
 Opening_hours = 1.5
 print(Opening_hours)
-Kurs = "Python"
-print(Kurs)
 time_for_one_task = (12/1.5)
 print(time_for_one_task)
+Kurs: ("Python","HommeWork 12","Opening_hours 1.5","time_for_one_task 0.125")
+print("Python","HommeWork 12","Opening_hours 1.5","time_for_one_task 0.125")
