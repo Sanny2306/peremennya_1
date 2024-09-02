9c80ec4198893789a21d5d672d5f20c921b55ff4
Sanny2306 <rastegaev.90@bk.ru>
2024-09-02 20:33:32 +0300

    исправленая домашняя работа в 1001 раз!

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
-name = "Sanny"
-print(name)
-HommeWork = 12
-print (HommeWork)
-Opening_hours = 1.5
-print(Opening_hours)
-time_for_one_task = (HommeWork/Opening_hours)
-print(time_for_one_task)
-Kurs: ("Python","HommeWork 12","Opening_hours 1.5","time_for_one_task 0.125")
-print("Python","HommeWork 12","Opening_hours 1.5","time_for_one_task 0.125")
+kurs = "Python"
+print(kurs)
+hommework = 12
+print (hommework)
+time = 1.5
+print (time)
+time_for_one_task = (time/hommework)
+print (time_for_one_task)
+print (f"Курс {kurs}, всего заданий {hommework},затрачено часов {time},среднее время выполнения {time_for_one_task}")
